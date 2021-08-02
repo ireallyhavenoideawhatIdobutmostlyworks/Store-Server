fbafe33ec3e1edc99c33d2d84e22dedfc1d97d4e
m.urbaniak <urbaniak.michal@yahoo.com>
2021-08-02 11:21:41 +0200

    small clean code

--- store/src/test/java/practice/store/product/ProductControllerTest.java
+++ store/src/test/java/practice/store/product/ProductControllerTest.java
@@ -65,7 +65,6 @@ class ProductControllerTest {
 
         productPayloadWithDiscount = DataFactoryProduct.createProductPayloadWithDiscount();
         productPayloadWithoutDiscount = DataFactoryProduct.createProductPayloadWithoutDiscount();
-
     }
 
 
