5efdc359a415b0f3adbae80d444cd129d2a41411
m.urbaniak <urbaniak.michal@yahoo.com>
2021-10-16 11:26:54 +0200

    wip

--- mail-service/src/main/java/practice/mailservice/MailServiceApplication.java
+++ mail-service/src/main/java/practice/mailservice/MailServiceApplication.java
@@ -2,14 +2,11 @@ package practice.mailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class MailServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MailServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MailServiceApplication.class, args);
+    }
 }
--- mail-service/src/main/java/practice/mailservice/utils/ClearDocsFolder.java
+++ mail-service/src/main/java/practice/mailservice/utils/ClearDocsFolder.java
@@ -2,8 +2,9 @@ package practice.mailservice.utils;
 
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -11,14 +12,14 @@ import java.io.IOException;
 
 @PropertySource("classpath:file.properties")
 @Component
-public class ClearDocsFolder {
+public class ClearDocsFolder implements ApplicationRunner {
 
     @Value("${docs.folder.path}")
     private String docsFolderPath;
 
 
-    @Scheduled(fixedRate = 30000)
-    public void cleanDirectory() throws IOException {
+    @Override
+    public void run(ApplicationArguments args) throws IOException {
         FileUtils.cleanDirectory(new File(docsFolderPath));
     }
 }
